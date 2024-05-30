15f0d5460db85af7ecd1724bbf087cc72254b21c
Raphael Olajide <Raphael.Olajide@signaturebankng.com>
2024-05-30 15:54:46 +0100

    sort not working at the moment

--- src/app/presentation/pages/Doctrine/dap/dap.component.ts
+++ src/app/presentation/pages/Doctrine/dap/dap.component.ts
@@ -1,13 +1,7 @@
 import { Component, ViewChild } from '@angular/core';
 import { DapService } from './Dap.service';
-import { StreamState } from 'src/app/core/domain/Interfaces/IStream.state';
-import { dapDTO } from 'src/app/core/domain/Models/dapDTO';
-import { productsData } from '../../dashboard/dashboard.component';
 import { MatPaginator } from '@angular/material/paginator';
-import { merge, of } from 'rxjs';
-import { startWith, switchMap } from 'rxjs/operators';
 import { MatTableDataSource } from '@angular/material/table';
-import { MatSort } from '@angular/material/sort';
 @Component({
   selector: 'app-dap',
   templateUrl: './dap.component.html',
@@ -19,9 +13,8 @@ export class DapComponent {
 
   dataSource!: MatTableDataSource<any> ;
   dataSize: number = 0;
-  displayedColumns: string[] = ['Audio', 'Topic', 'Name', 'Type'];
+  displayedColumns: string[] = ['S/N','Audio', 'Topic', 'Name', 'Type'];
   @ViewChild(MatPaginator, { static: true }) paginator!: MatPaginator;
-  @ViewChild(MatSort, { static: true }) sort!: MatSort;
   
   /**
    *
@@ -37,14 +30,13 @@ export class DapComponent {
 
   getDapFiles(){
   // get media files
-  this.dapService.getFiles().subscribe((files:any) => {
-    this.dataSize = this.files.length;
-    this.files = files;
-    this.dataSource = new MatTableDataSource(this.files);
-    this.dataSource.paginator = this.paginator;
-    this.dataSource.sort = this.sort;
+    this.dapService.getFiles().subscribe((files:any) => {
+      this.dataSize = this.files.length;
+      this.files = files.reverse();
+      this.dataSource = new MatTableDataSource(this.files);
+      this.dataSource.paginator = this.paginator;
 
-  });
+    });
   }
 
 
